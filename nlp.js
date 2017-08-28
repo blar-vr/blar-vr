0d03ceb93866a3e9cc95ba536816a9bb7f7185be
Dan Zajdband <dan.zajdband@gmail.com>
2017-08-28 20:08:47 -0300

    Initial commit

---
+++ nlp.js
@@ -0,0 +1,365 @@
+
+/**
+ * Module constants
+ */
+
+var TYPES = {
+  'audio': { alias: ['sound', '🔊'] },
+  'panorama': { alias: ['🌅'] },
+  'image': { alias: ['foto', 'picture', 'imagen'] },
+  'text': { alias: ['texto', '📝'] },
+  'videosphere': { alias: ['video esfera', '🎥'] },
+  'video': { alias: [] },
+  'seconds': { alias: ['segundos', '⏲'] },
+  'voiceover': { alias: ['voz en off', '📢'] },
+  'chart': { alias: ['gráfico', '📊'] },
+  'background': { alias: ['fondo'] },
+  'model': { alias: ['modelo'] },
+  'sky': { alias: ['cielo'] }
+}
+
+var ENTITIES_REGEX = new RegExp('(^|\\s|;|\\.|,|:)(' + Object.keys(TYPES).map(function (type) { return TYPES[type].alias.concat(type).join('|') }).join('|') + ')(\\s|$|;|\\.|,|:)', 'gi')
+var LOCATION_REGEX = /right|left|behind|front|above|below|atrás|frente|izquierda|derecha|arriba|abajo/gi
+var SIZE_REGEX = /tiny|small|large|huge|diminuto|pequeño|grande|enorme/i
+var SUN_POSITION_REGEX = /sunrise|sunset|morning|noon|afternoon|evening|night|amanecer|atardecer|mañana|mediodía|tarde|noche/i
+var LATLON_REGEX = /\-?\d+\.\d+,\s*\-?\d+\.\d+/
+var MARKER_REGEX = /marker|marcador/i
+var SCENE_LINK_REGEX = /(first|second|third|fourth|fifth|sixth|primera|segunda|tercera|cuarta|quinta|sexta) (scene|escena)/i
+var SCENE_INDEXES = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'primera', 'segunda', 'tercera', 'cuarta', 'quinta', 'sexta']
+
+module.exports = function (str) {
+  return str
+  .split('\n')
+  .map(getObjects)
+  .filter(function (obj) { return obj.length })
+}
+
+function getObjects (p) {
+  var entities = []
+  var entity
+
+  // Iterate over the paragraph and extract entities
+  while ((entity = ENTITIES_REGEX.exec(p)) !== null) {
+    entities.push({ type: entity[2].trim(), index: entity.index })
+  }
+
+  // Extract attributes for each entity
+  return entities.map(function (entity, i) {
+    // for backwards words I'm going back to the previous entity and add the length
+    var sp = i !== 0
+    ? p.substring(entities[i - 1].index + entities[i - 1].type.length, entities[i].index + entities[i].type.length + 1)
+    : p.substring(0, entities[i].index + entities[i].type.length + 1)
+
+    // special case for duration
+    var match
+    if (entity.type === 'seconds' || entity.type === 'second' || entity.type === '⏲' || entity.type === 'segundo' || entity.type === 'segundos') {
+      match = sp.match(/[0-9]+ (⏲|seconds?|segundos)/)
+      if (!match) return false
+
+      return {
+        type: 'duration',
+        value: parseInt(match[0].replace(/(⏲|seconds?|segundos)/, ''), 10)
+      }
+    } else if (entity.type === 'background') {
+      match = sp.match(/(#[a-fA-F0-9]{3,6}|\w+) background/i)
+      return {
+        type: 'background',
+        color: match[0].replace(' background', '')
+      }
+    }
+
+    // Get the portion of the text relative to this entity
+    sp = i === entities.length - 1
+    ? p.substring(entity.index)
+    : p.substring(entity.index, entities[i + 1].index + 1)
+
+    var str = sp
+    switch (entity.type) {
+      case 'audio':
+      case 'sound':
+      case '🔊':
+        var audioUrl = getUrl(str)
+        var audioQuote = getQuote(str)
+        if (!(audioUrl || audioQuote)) return
+        return {
+          type: 'audio',
+          src: audioUrl,
+          text: !audioUrl && audioQuote,
+          position: getPosition(str)
+        }
+      case 'voiceover':
+      case 'voz en off':
+      case '📢':
+        var voiceoverQuote = getQuote(str)
+        if (!voiceoverQuote) return
+        return {
+          type: 'voiceover',
+          text: voiceoverQuote
+        }
+      case 'chart':
+      case 'gráfico':
+      case '📊':
+        var chartUrl = getUrl(str)
+        var chartMarker = getMarker(str)
+        if (!chartUrl) return
+        var chartPos = getPosition(str, 10, 10)
+        return {
+          type: 'chart',
+          src: chartUrl,
+          position: chartPos,
+          scale: getSize(str),
+          marker: chartMarker
+        }
+      case 'panorama':
+      case '🌅':
+        var panoUrl = getUrl(str)
+        var panoQuote = getQuote(str)
+        var panoLatLon = getLatLon(str)
+        if (!(panoUrl || panoQuote || panoLatLon)) return
+        return {
+          type: 'panorama',
+          src: panoUrl,
+          text: !panoUrl && panoQuote,
+          latlon: !(panoUrl || panoQuote) && panoLatLon
+        }
+      case 'video':
+        var videoUrl = getUrl(str)
+        if (!videoUrl) return
+        var videoPos = getPosition(str)
+        return {
+          type: 'video',
+          src: videoUrl,
+          position: videoPos,
+          scale: getSize(str),
+          link: getLink(str),
+          marker: getMarker(str)
+        }
+      case 'videosphere':
+      case 'video esfera':
+      case '🎥':
+        var sphereUrl = getUrl(str)
+        if (!sphereUrl) return
+        return {
+          type: 'videosphere',
+          src: sphereUrl
+        }
+      case 'image':
+      case 'picture':
+      case 'foto':
+      case 'imagen':
+        var imgUrl = getUrl(str)
+        var imgQuote = getQuote(str)
+        if (!(imgUrl || imgQuote)) return
+        var imgPos = getPosition(str)
+        return {
+          type: 'image',
+          src: imgUrl,
+          text: !imgUrl && imgQuote,
+          position: imgPos,
+          scale: getSize(str),
+          link: getLink(str),
+          marker: getMarker(str)
+        }
+      case 'text':
+      case 'texto':
+      case '📝':
+        var textQuote = getQuote(str)
+        if (!textQuote) return
+        var textPos = getPosition(str /*, 14, textQuote.length / 30*/)
+        return {
+          type: 'text',
+          text: textQuote,
+          position: textPos,
+          scale: getSize(str).map(function (el) { return el * 25 }),
+          marker: getMarker(str)
+        }
+      case 'sky':
+      case 'cielo':
+        return {
+          type: 'sky',
+          position: getSunPosition(str)
+        }
+      case 'model':
+      case 'modelo':
+        var url = getUrl(str, /\.obj$/)
+        var mtl = null
+        var ext = ''
+        if (url) {
+          mtl = getUrl(str, /\.mtl$/)
+          ext = ['obj']
+        } else {
+          url = getUrl(str)
+          if (url) {
+            ext = url.split('.')
+          }
+        }
+
+        if (!url) return
+        var modelPos = convertModelPosition(getPosition(str))
+        return {
+          type: 'model',
+          src: url,
+          mtl: mtl,
+          extension: ext[ext.length - 1],
+          position: modelPos,
+          scale: getSize(str),
+          marker: getMarker(str)
+        }
+      case 'fondo':
+        match = sp.match(/fondo (#[a-fA-F0-9]{3,6})/i)
+        if (!(match && match.length >= 2)) return
+        return {
+          type: 'background',
+          color: match[1]
+        }
+    }
+
+    return entity
+  })
+  .filter(function (entity) { return !!entity })
+}
+
+function convertModelPosition (pos) {
+  return [pos[0], pos[1] - 3, pos[2]]
+}
+
+function getUrl (str, validate) {
+  var match = str.match(/https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
+  if (!(match && match.length)) return
+  if (validate) {
+    match = match.filter(function (m) { return validate.test(m) })
+  }
+  if (!match.length) return
+
+  return match[0]
+}
+
+function getQuote (str) {
+  var match = str.match(/".*"/)
+  if (!(match && match.length)) return ''
+  return match[0].replace(/"/g, '')
+}
+
+function getPosition (str, width, height) {
+  width = width || 0
+  height = height || 0
+
+  var positions = []
+  var position = null
+  var input = str
+  var distance = null
+  while ((position = LOCATION_REGEX.exec(str)) !== null) {
+    positions.push({ type: position[0], index: position.index })
+    distance = input.substring(0, position.index).match(/(\d*(\.\d+)?) (meter|metro)/i)
+    positions[positions.length - 1].distance = distance && distance.length ? parseFloat(distance[0].replace(/meter|metro/i, '')) : 0
+    input = input.slice(position.index)
+  }
+  return getAbsPos(positions.length ? positions : [{type: 'front', distance: 0}], width, height)
+}
+
+function getSize (str) {
+  var match = str.match(SIZE_REGEX)
+  return getAbsSize(match && match.length ? match[0] : 'normal')
+}
+
+function getLink (str) {
+  var match = str.match(SCENE_LINK_REGEX)
+  if (!(match && match.length >= 2 && SCENE_INDEXES.indexOf(match[1]) !== -1)) return
+  return SCENE_INDEXES.indexOf(match[1]) % (SCENE_INDEXES.length / 2)
+}
+
+function getAbsPos (positions, width, height) {
+  var xSize = -0.5 * width
+  var ySize = -0.5 * height
+  var initialPosition = [0, 1.6, 0]
+  for (var i = 0; i < positions.length; i++) {
+    switch (positions[i].type) {
+      case 'left':
+      case 'izquierda':
+        initialPosition[0] -= positions[i].distance || 7
+        initialPosition[1] += ySize
+        initialPosition[2] -= xSize
+        break
+      case 'right':
+      case 'derecha':
+        initialPosition[0] += positions[i].distance || 7
+        initialPosition[1] += ySize
+        initialPosition[2] += xSize
+        break
+      case 'above':
+      case 'arriba':
+        initialPosition[0] += xSize
+        initialPosition[1] += positions[i].distance || 6
+        break
+      case 'below':
+      case 'abajo':
+        initialPosition[0] += xSize
+        initialPosition[1] -= positions[i].distance || 3
+        break
+      case 'behind':
+      case 'atrás':
+        initialPosition[0] -= xSize
+        initialPosition[1] += ySize
+        initialPosition[2] += positions[i].distance || 8
+        break
+      case 'front':
+      case 'frente':
+      default:
+        initialPosition[0] += xSize
+        initialPosition[1] += ySize
+        initialPosition[2] -= positions[i].distance || 8
+        break
+    }
+  }
+  return initialPosition
+}
+
+function getAbsSize (str) {
+  switch (str) {
+    case 'tiny':
+    case 'diminuto':
+      return [1 / 3, 1 / 3, 1 / 3]
+    case 'small':
+    case 'pequeño':
+      return [1 / 2, 1 / 2, 1 / 2]
+    case 'large':
+    case 'grande':
+      return [2, 2, 2]
+    case 'huge':
+    case 'enorme':
+      return [3, 3, 3]
+    default:
+      return [1, 1, 1]
+  }
+}
+
+function getSunPosition (str) {
+  var match = str.match(SUN_POSITION_REGEX)
+  return getAbsSunPos(match && match.length ? match[0] : null)
+}
+
+function getAbsSunPos (str) {
+  switch (str) {
+    case 'noon': case 'mediodía':
+      return [0, 1, 0]
+    case 'sunrise': case 'sunset': case 'amanecer': case 'atardecer':
+      return [0, 0, -1]
+    case 'evening': case 'night': case 'noche':
+      return [10, -1, 0]
+    case 'morning': case 'afternoon': case 'mañana': case 'tarde':
+    default:
+      return [1, 0.5, 0]
+  }
+}
+
+function getLatLon (str) {
+  var match = str.match(LATLON_REGEX)
+  if (!(match && match.length)) return null
+  return match[0].split(',').map(parseFloat)
+}
+
+function getMarker (str) {
+  var match = str.match(MARKER_REGEX)
+  if (!(match && match.length)) return null
+  return true
+}
---
+++ story-builder.js
@@ -0,0 +1,346 @@
+
+const EXTERNAL_URLS = {
+  chart: 'https://cdn.rawgit.com/impronunciable/aframe-chartbuilder-component/4693e47a/dist/aframe-chartbuilder-component.min.js',
+  ply: 'https://rawgit.com/donmccurdy/aframe-extras/v2.1.1/dist/aframe-extras.loaders.min.js',
+  sky: 'https://rawgit.com/ngokevin/kframe/master/components/sun-sky/dist/aframe-sun-sky.min.js',
+  arjs: 'https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js'
+}
+
+const MANUAL_PLAY_TYPES = ['video', 'videosphere', 'audio']
+
+module.exports = story =>
+`
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="twitter:card" content="player">
+    <meta name="twitter:site" content="@guri_vr">
+    <meta name="twitter:title" content="${story.title}">
+    <meta name="twitter:image" content="https://s3.amazonaws.com/gurivr/logo_twitter.png">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:player" content="https://s3.amazonaws.com/gurivr/s/${story._id}.html">
+    <meta name="twitter:player:width" content="400">
+    <meta name="twitter:player:height" content="370">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <title>${story.title}</title>
+    <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+    <script src="https://rawgit.com/ngokevin/aframe-look-at-component/master/dist/aframe-look-at-component.min.js"></script>
+    ${renderExternalUrls(story)}
+    <style>
+      html, body, #root, #arVideo {
+        background-color: ${story.mode === 'ar' ? 'transparent' : '#000'};
+        height: 100vh;
+        width: 100vw;
+        color: #fff;
+        font-size: 1.6em;
+        text-align: center;
+      }
+
+      #root {
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-pack: center;
+        -webkit-justify-content: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -webkit-align-items: center;
+        -ms-flex-align: center;
+        align-items: center;
+      }
+
+      #arVideo {
+        width: 100%    !important;
+        max-width: 100%    !important;
+        height: auto   !important;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1000;
+      }
+    </style>
+  </head>
+  <body>
+    <a-scene>
+      <a-entity camera="userHeight: 1.6" look-controls wasd-controls>
+        <a-entity id="cursor"
+                  cursor="fuse: false; maxDistance: 30; timeout: 500"
+                  position="0 0 -5"
+                  geometry="primitive: ring; radiusInner: 0.2; radiusOuter: 0.3"
+                  material="color: white; shader: flat">
+        </a-entity>
+      </a-entity>
+      <a-assets>${story.chapters.map(renderChapterAssets).filter(assets => assets.trim().length)}</a-assets>
+      ${story.chapters.map(renderChapter).join('\n')}
+    </a-scene>
+    <div id="root" style="background: #000; z-index: 999 !important; cursor: pointer; position: absolute; top: 0; left: 0;" onclick="javascript:start()"><svg style="width:50px;height:50px" viewBox="0 0 24 24"><path fill="#FFFFFF" d="M8,5.14V19.14L19,12.14L8,5.14Z" /></svg></div>
+    <script>
+      ${renderScript(story)}
+    </script>
+  </body>
+</html>
+`
+
+const renderChapterAssets = (chapter, i) => chapter
+.map((obj, key) => (obj.src && renderObjectAsset(obj, i, key)) || null)
+.filter(asset => !!asset)
+.join('\n')
+
+const renderObjectAsset = (obj, i, j) => {
+  switch (obj.type) {
+    case 'panorama':
+    case 'image':
+      return `<img src="${obj.src}" id="asset-${i}-${j}" crossorigin="anonymous">`
+    case 'video':
+    case 'videosphere':
+      return `<video src="${obj.src}" id="asset-${i}-${j}" class="chapter-${i}" crossorigin="anonymous" autoplay="false"></video>`
+    case 'audio':
+      return `<audio src="${obj.src}" id="asset-${i}-${j}" class="chapter-${i}" crossorigin="anonymous"></audio>`
+    case 'model':
+      const ext = obj.src.split('.')
+      return ext[ext.length - 1] === 'obj'
+      ? `<a-asset-item src="${obj.src}" id="asset-${i}-${j}-obj"  crossorigin="anonymous"></a-asset-item><a-asset-item src="${obj.mtl}" id="asset-${i}-${j}-mtl"  crossorigin="anonymous"></a-asset-item>`
+      : `<a-asset-item src="${obj.src}" id="asset-${i}-${j}"  crossorigin="anonymous"></a-asset-item>`
+  }
+}
+
+const renderChapter = (chapter, i) => `
+  <a-entity class="chapter${needsCursor(chapter) ? ' cursor' : ''}" visible="false">
+    ${chapter.map((obj, key) => renderObject(obj, i, key)).join('\n')}
+  </a-entity>
+`
+
+const renderObject = (obj, i, j) => {
+  let str = ''
+  if (obj.marker) {
+    marker = {
+      position: obj.position,
+      scale: obj.scale
+    }
+    obj.position = ['']
+    obj.scale = ['']
+  }
+  switch (obj.type) {
+    case 'text':
+      str = `<a-entity look-at="0 1.6 0" scale="${obj.scale.join(' ')}" position="${obj.position.join(' ')}" text="value: ${obj.text}; font: roboto; align: center; anchor: center;"></a-entity>`
+      break
+    case 'panorama':
+      str = `<a-sky src="#asset-${i}-${j}"></a-sky>`
+      break
+    case 'background':
+      str = `<a-sky color="${obj.color}"></a-sky>`
+      break
+    case 'videosphere':
+      str = `<a-videosphere src="#asset-${i}-${j}"></a-videosphere>`
+      break
+    case 'video':
+      str = `<a-video look-at="0 1.6 0" scale="${obj.scale.join(' ')}" width="10" height="6" position="${obj.position.join(' ')}" src="#asset-${i}-${j}"></a-video>`
+      break
+    case 'image':
+      str = `<a-image look-at="0 1.6 0" scale="${obj.scale.join(' ')}" width="5" height="5" position="${obj.position.join(' ')}" src="#asset-${i}-${j}"></a-image>`
+      break
+    case 'audio':
+      str = `<a-entity position="${obj.position.join(' ')}" sound="src: #asset-${i}-${j}; autoplay: false;"></a-entity>`
+      break
+    case 'sky':
+      str = `<a-sun-sky material="sunPosition: ${obj.position.join(' ')}"></a-sun-sky>`
+      break
+    case 'chart':
+      str = `<a-entity position="${obj.position.join(' ')}" chartbuilder="src: ${obj.src}; scale: ${obj.scale.join(' ')};"></a-entity>`
+      break
+    case 'model':
+      switch (obj.extension) {
+        case 'ply':
+          str = `<a-entity look-at="0 1.6 0" ply-model="src: #asset-${i}-${j}" position="${obj.position.join(' ')}"></a-entity>`
+          break
+        case 'obj':
+          str = `<a-entity look-at="0 1.6 0" scale="${obj.scale.join(' ')}" position="${obj.position.join(' ')}" obj-model="obj: #asset-${i}-${j}-obj; mtl:  #asset-${i}-${j}-mtl;"></a-entity>`
+          break
+        case 'dae':
+        default:
+          str = `<a-collada-model look-at="0 1.6 0" scale="${obj.scale.join(' ')}" position="${obj.position.join(' ')}" src="#asset-${i}-${j}"></a-collada-model>`
+          break
+      }
+  }
+  if (!obj.marker) {
+    return str
+  } else {
+    return `
+      <a-marker preset="hiro">
+        ${str}
+      </a-marker>
+    `
+  }
+}
+
+const renderScript = story => {
+  const chapters = story.chapters
+  const times = []
+  const voices = []
+  chapters.forEach(chapter => {
+    var time = chapter.filter(obj => obj.type === 'duration')
+    times.push(time.length ? time[0].value : null)
+
+    var voice = chapter.filter(obj => obj.type === 'voiceover')
+    voices.push(voice.length ? voice[0].text : null)
+  })
+
+  return `
+    var times = ${JSON.stringify(times)};
+    var voices = ${JSON.stringify(voices)};
+
+    var chapters = document.querySelectorAll('.chapter');
+    var cursor = document.querySelector('#cursor')
+    var prevIdx = null;
+    var cancelNext;
+
+    function nextChapter(i) {
+      if(i >= chapters.length) return end();
+
+      clearTimeout(cancelNext);
+
+      var curr = chapters[i];
+      var prev;
+
+      if (prevIdx != null) {
+        prev = chapters[prevIdx];
+      }
+
+      playVoiceover(i);
+
+      if (curr.classList.contains('cursor')) {
+        cursor.setAttribute('visible', 'true');
+      } else {
+        cursor.setAttribute('visible', 'false');
+      }
+
+      if (prevIdx != null) {
+        prev.setAttribute('visible', false);
+      }
+      curr.setAttribute('visible', true);
+
+      if (prevIdx != null) {
+        var assets = document.querySelectorAll('.chapter-' + prevIdx);
+        for (var j = 0; j < assets.length; j++) {
+          assets[j] && assets[j].pause && assets[j].pause();
+        }
+      }
+
+      assets = document.querySelectorAll('.chapter-' + i);
+      for (var j = 0; j < assets.length; j++) {
+        assets[j] && assets[j].play && assets[j].play();
+      }
+
+      if (times[i]) {
+        cancelNext = setTimeout(function() {
+          nextChapter(i + 1);
+        }, times[i] * 1000);
+      }
+
+      prevIdx = i
+    }
+
+    function start() {
+      document.querySelector('a-scene').setAttribute('vr-mode-ui', 'enabled: true');
+      var $root = document.querySelector('#root');
+      $root.parentNode.removeChild($root);
+      nextChapter(0);
+    }
+
+    function end() {
+      document.body.innerHTML = '<div id="root" style="background: #000; cursor: pointer" onclick="javascript:window.location = window.location"><svg fill="#FFFFFF" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/></svg></div>';
+    }
+
+    function playVoiceover(i) {
+      if (voices[i]) {
+        try {
+          speechSynthesis.cancel();
+          var txt = new SpeechSynthesisUtterance(voices[i]);
+          speechSynthesis.speak(txt);
+        } catch (err) {}
+      }
+    }
+    ${!needsManualPlay(story.chapters) ? 'start()' : ''}
+
+    ${story.mode === 'ar' ? renderARScript() : ''}
+  `
+}
+
+const renderARScript = () =>
+`
+  var sky = document.querySelectorAll('a-sky, a-panorama, a-videosphere');
+
+  for (var i = 0; i < sky.length; i++) {
+    sky[i].setAttribute('visible', 'false');
+  }
+
+  var scene = document.querySelector('a-scene');
+  scene.setAttribute('arjs', 'sourceType: webcam;')
+  scene.setAttribute('embedded', 'embedded')
+`
+
+const renderExternalUrls = story => {
+  const loaded = {}
+  const urls = []
+  if (story.mode === 'ar') {
+    urls.push(EXTERNAL_URLS.arjs)
+  }
+  story.chapters.forEach(chapter => chapter.forEach(entity => {
+    switch (entity.type) {
+      case 'chart':
+        if (!loaded.chart) {
+          urls.push(EXTERNAL_URLS.chart)
+        }
+        break
+      case 'model':
+        if (!loaded.ply && entity.extension === 'ply') {
+          urls.push(EXTERNAL_URLS.ply)
+        }
+        break
+      case 'sky':
+        if (!loaded.sky) {
+          urls.push(EXTERNAL_URLS.sky)
+        }
+        break
+    }
+  }))
+
+  return urls.map(url => `<script src="${url}"></script>`).join('\n')
+}
+
+/**
+ * Render click event
+ */
+
+const renderEvents = obj => ` onclick="nextChapter(${obj.link})"`
+
+/**
+ * Checks if needs a play button for audios and videos
+ */
+
+const needsManualPlay = chapters => {
+  for (let i = 0; i < chapters.length; i++) {
+    for (let j = 0; j < chapters[i].length; j++) {
+      if (MANUAL_PLAY_TYPES.indexOf(chapters[i][j].type) !== -1) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+/**
+ * Check if a cursor is needed for this scene. If there is at least
+ * one link, then we need a cursor for it
+ */
+
+const needsCursor = chapter => {
+  for (let i = 0; i < chapter.length; i++) {
+    if (typeof chapter[i].link !== 'undefined') return true
+  }
+}
